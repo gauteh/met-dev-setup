9d0ec759a2bd9af749e8798a1591031061b96f42
Gaute Hope <gauteh@met.no>
2026-04-24 16:55:52 +0200

    denycurrent

--- main.sh
+++ main.sh
@@ -108,5 +108,7 @@ mamba activate neovim
 mkdir dev
 cd dev
 
+git config --global receive.denyCurrentBranch warn
+
 git clone https://github.com/gauteh/max-m10s-rs.git
 git clone https://github.com/gauteh/sfy.git
